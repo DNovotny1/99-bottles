d79073a4d15e385a56a7be23fb42824bf2f57e19
DNovotny1 <DNovotny1@users.noreply.github.com>
2018-01-29 17:28:04 -0600

    addedIf
    added an if statement for assignment

--- 99bottles.cpp
+++ 99bottles.cpp
@@ -5,14 +5,22 @@ int main()
 {
 	int i = 99;
 	
-	//writes bottle of beer, decrements, repeats
-	while(i > 1)
+	//writes bottle of beer lyrics, decrements, repeats
+	while(i > 0)
 	{
-		cout << i <<  " bottles of beer on the wall, " << i << " bottles of beer, take one down, pass it around, " << i << " bottles of beer on the wall.\n" ;
+		//checks if 1 is singular and writes in correct format
+		if (i == 1)
+		{
+			cout << i <<  " bottle of beer on the wall, " << i << " bottle of beer, take it down, pass it around, " << i << " bottle of beer on the wall.\n" ;
+		}
+		else
+		{			
+			cout << i <<  " bottles of beer on the wall, " << i << " bottles of beer, take one down, pass it around, " << i << " bottles of beer on the wall.\n" ;
+		}
 		i--;
+		
 	}
-//writes lyric one more time for singular format 
-cout << i <<  " bottle of beer on the wall, " << i << " bottle of beer, take it down, pass it around, " << i << " bottle of beer on the wall.\n" ;
+
 cin >> i;
 return 0;
 }	
